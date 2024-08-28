e1e7e930cece6682be90516626925905bc3a9031
Kazuma1102 <k.sugiyama.24.7.1555@4nonome.com>
2024-08-29 02:39:07 +0900

    python入門問2

--- pi.py
+++ pi.py
@@ -2,6 +2,10 @@ text = """
     How I want a drink, alcoholic of course, after the heavy chapters involving
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
+a=list(map(len,["How","I","want","a","drink","alcoholic","of","course","after","the","heavy","chapters","involving",
+    "quantum","mechanics", "All","of" ,"thy" ,"geometry", "Herr", "Planck", "is" ,"fairly" ,"hard"]))
 
+pai="".join(map(str,a))
+print(pai)
 # TODO
 
