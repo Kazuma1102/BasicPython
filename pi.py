ade36b066f8e56afc185068cff5cf87e7b4461cd
Kazuma1102 <k.sugiyama.24.7.1555@4nonome.com>
2024-11-15 21:14:50 +0900

    python 入門変更1

--- pi.py
+++ pi.py
@@ -2,10 +2,9 @@ text = """
     How I want a drink, alcoholic of course, after the heavy chapters involving
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
-a=list(map(len,["How","I","want","a","drink","alcoholic","of","course","after","the","heavy","chapters","involving",
-    "quantum","mechanics", "All","of" ,"thy" ,"geometry", "Herr", "Planck", "is" ,"fairly" ,"hard"]))
+words = text.replace(",", "").replace(".", "").split()
 
-pai="".join(map(str,a))
-print(pai)
+result = "".join(map(str, map(len, words)))
+print(result)
 # TODO
 
