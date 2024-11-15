f893285b6c43fc0aac8b2bd81b3101e040c1546c
Kazuma1102 <k.sugiyama.24.7.1555@4nonome.com>
2024-11-15 20:44:51 +0900

    制御構文

--- euclid.py
+++ euclid.py
@@ -1,4 +1,44 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+#制御構文
+try:
+    
 
-# TODO
+    a = int(input("8177"))
+    b = int(input("315"))
+
+    
+    while b != 0:
+        a, b = b, a % b
+        print(b)
+
+except ValueError:
+    print("エラー: 数値を入力してください。")
+
+
+
+# 関数
+#ユークリッドの互除法
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+result = gcd(48, 18)
+print(result)  
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+# 互いに素
+def are_coprime(a, b):
+    return gcd(a, b) == 1
+
+# 使用例
+a = 15
+b = 28
+
+if are_coprime(a, b):
+    print(f"{a} と {b} は互いに素です。")
+else:
+    print(f"{a} と {b} は互いに素ではありません。")
--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,64 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+#制御構文
+n=61
+D=True
+for i in range(2,n):
+    if n % i ==0: 
+               D=True
+               break
+    else :
+        D=False
+        
 
-# TODO
+if D:
+     print(f"{n}は素数ではありません")
+else:
+     print(f"{n}は素数です")
+n=10       
+
+
+D=True
+for i in range(2,n):
+    if n % i ==0: 
+               D=True
+               break
+    else :
+        D=False
+        
+
+if D:
+     print(f"{n}は素数ではありません")
+else:
+     print(f"{n}は素数です")
+
+#関数
+E=True
+m=61
+def is_prime(m):
+  
+    if m <= 1:
+        return False
+    for i in range(2, m):
+        if m % i == 0:
+            return False
+    return True
+E=is_prime(m)
+if E:
+     print(f"{m}は素数です")
+else:
+     print(f"{m}は素数ではありません")
+
+E=True
+m=10
+def is_prime(m):
+  
+    if m <= 1:
+        return False
+    for i in range(2, m):
+        if m % i == 0:
+            return False
+    return True
+E= is_prime(m)
+if E:
+     print(f"{m}は素数です")
+else:
+     print(f"{m}は素数ではありません")
