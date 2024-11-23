e70f47fb05a883249a8623fb8b2cec64e8ba504d
Kazuma1102 <k.sugiyama.24.7.1555@4nonome.com>
2024-11-24 02:09:15 +0900

    制御構文直し3

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,26 +1,13 @@
 #制御構文
-<<<<<<< HEAD
 from math import sin,pi
-
-h=(pi/2/100)
-S=0
+a = 0
+b = pi / 2
+n = 100
+h=(b/n)
 for k in range(1,101):
          t=(h/2)*(sin((k-1)*h)+sin(k*h))
-         S+=t
+         a+=t
 
-print(S)
-
-=======
-from math import sin
-from math import pi
-
-h=(pi/2/100)
-S=0
-for k in range(100):
-    t=(h/2)*(sin((k-1)*h)+sin(k*h))
-    S+=t
-
-print(S)
+print(a)
 
 
->>>>>>> f8631dfb736b9475037882e09acab4677b88dab5
