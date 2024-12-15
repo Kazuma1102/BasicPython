fe0309acc3599897e055260f3048ee3e6aab69be
Kazuma1102 <k.sugiyama.24.7.1555@4nonome.com>
2024-12-15 18:16:15 +0900

    python関数

--- prime_number.py
+++ prime_number.py
@@ -1,4 +1,32 @@
-a = input("aの値を入力: ")
-b = input("bの値を入力: ")
+#関数
+E=True
+m=61
+def is_prime(m):
+  
+    if m <= 1:
+        return False
+    for i in range(2, m):
+        if m % i == 0:
+            return False
+    return True
+E=is_prime(m)
+if E:
+     print(f"{m}は素数です")
+else:
+     print(f"{m}は素数ではありません")
 
-# TODO
+E=True
+m=10
+def is_prime(m):
+  
+    if m <= 1:
+        return False
+    for i in range(2, m):
+        if m % i == 0:
+            return False
+    return True
+E= is_prime(m)
+if E:
+     print(f"{m}は素数です")
+else:
+     print(f"{m}は素数ではありません")
