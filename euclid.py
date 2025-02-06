f3ee5a709e451c65b9577f4aa9c378794a7233af
Kazuma1102 <k.sugiyama.24.7.1555@4nonome.com>
2025-02-07 07:15:45 +0900

    pi.py_2

--- euclid.py
+++ euclid.py
@@ -1,6 +1,9 @@
 # 関数
 #ユークリッドの互除法
 
+
+
+
 def gcd(a, b):
     while b != 0:
         a, b = b, a % b
@@ -10,20 +13,11 @@ result = gcd(48, 18)
 print(result)  
 
 
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
-
-
-def are_coprime(a, b):
-    return gcd(a, b) == 1
-
 #互いに素
 a = 15
 b = 28
 
 if are_coprime(a, b):
-    print(f"{a} と {b} は互いに素です。")
+    print(f"{a} と {b} は互いに素です")
 else:
-    print(f"{a} と {b} は互いに素ではありません。")
+    print(f"{a} と {b} は互いに素ではありません")
